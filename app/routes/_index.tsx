5f419e9fef0a5cca1006b0f9deb8efe7c3e3c459
Adosh Singh <adoshbhanoo@gmail.com>
2024-04-22 18:21:19 +0530

    #01 AS: setup boilerplate code for the app

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className=" bg-blue-400">
+      Hello World
+    </div>
+  );
+}
